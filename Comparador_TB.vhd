a6acbbca6db22e4b48dfd6f2b5fd8ca610cc9589
Kaique SC <kaiquecs.bh@gmail.com>
2021-03-22 20:45:45 -0300

    Tb do comparador recente atualizado

--- Comparador_TB.vhd
+++ Comparador_TB.vhd
@@ -1,48 +1,69 @@
--- Testbench automatically generated online
--- at https://vhdl.lapinoo.net
--- Generation date : 22.3.2021 20:03:16 UTC
-
 library ieee;
 use ieee.std_logic_1164.all;
+use std.textio.all;
+use ieee.std_logic_textio.all;
 
-entity tb_comparator_VHDL is
-end tb_comparator_VHDL;
+entity tb_comparator_8bits is 
+end tb_comparator_8bits;
 
-architecture tb of tb_comparator_VHDL is
+architecture tb of tb_comparator_8bits is
 
-    component comparator_VHDL
-        port (A        : in std_logic;
-              B        : in std_logic;
-              A_less_B : out std_logic);
+    component comparator_8bits is
+        port (
+            A : in std_logic_vector(7 downto 0);
+            B : in std_logic_vector(7 downto 0);
+            A_less_B : out std_logic  
+        );
     end component;
 
-    signal A        : std_logic;
-    signal B        : std_logic;
+    signal A : std_logic_vector(7 downto 0);
+    signal B : std_logic_vector(7 downto 0);
     signal A_less_B : std_logic;
 
-begin
+    -- Sinais para teste
+    signal TESTE_LESS : std_logic;
 
-    dut : comparator_VHDL
-    port map (A        => A,
-              B        => B,
-              A_less_B => A_less_B);
+    file input_file : text;
 
-    stimuli : process
-    begin
-        -- EDIT Adapt initialization as needed
-        A <= '0';
-        B <= '0';
+begin 
+    dut: entity work.comparator_8bits 
+        port map(
+            A => A,
+            B => B,
+            A_less_B => A_less_B
+        );
+    
+    tb_proc: process
+    variable input_file_read : line; 
+    variable input_file_col1 : std_logic_vector(7 downto 0); -- A 
+    variable input_file_col2 : std_logic_vector(7 downto 0); -- B
+    variable input_file_col3 : std_logic; -- A_less_B
+    variable input_file_space : character; 
 
-        -- EDIT Add stimuli here
+    begin 
+        file_open(input_file, "file_comparador.txt", read_mode);
 
+        while not endfile(input_file) loop 
+            -- Entrada de dados do arquivo
+            readline(input_file,input_file_read);
+            read(input_file_read,input_file_col1);
+            read(input_file_read,input_file_space);
+            read(input_file_read,input_file_col2);
+            read(input_file_read,input_file_space);
+            read(input_file_read,input_file_col3);
+
+            -- Associação 
+            A <= input_file_col1;
+            B <= input_file_col2;
+            TESTE_LESS <= input_file_col3;
+
+            -- Verificação dos resultados
+            assert A_less_B = TESTE_LESS report "A nao e maior que B" severity note;
+
+            wait for 15 ns;
+        end loop;
+
+        file_close(input_file);
         wait;
-    end process;
-
+    end process tb_proc;
 end tb;
-
--- Configuration block below is required by some simulators. Usually no need to edit.
-
-configuration cfg_tb_comparator_VHDL of tb_comparator_VHDL is
-    for tb
-    end for;
-end cfg_tb_comparator_VHDL;
