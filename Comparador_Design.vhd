683669738456dc5b8b7605572623216e504877ee
Rafael-Armond <34943649+Rafael-Armond@users.noreply.github.com>
2021-03-22 17:09:09 -0300

    Mudanças no comparador

--- Comparador_Design.vhd
+++ Comparador_Design.vhd
@@ -1,20 +1,68 @@
+-- Comparador de 2 bits
 library IEEE;
 use IEEE.std_logic_1164.all;
 entity comparator_VHDL is
   port (
-        A,B: in std_logic_vector(1 downto 0); 
+        A,B: in std_logic; 
         A_less_B: out std_logic 
   );
 end comparator_VHDL;
 architecture comparator_structural of comparator_VHDL is
-signal temp1,temp2,temp3: std_logic; 
 
 begin
 
- temp1 <= (not A(0)) and (not A(1)) and B(0);
- temp2 <= (not A(1)) and B(1);
- temp3 <= (not A(0)) and B(1) and B(0);
-
- A_less_B <= temp1 or temp2 or temp3;
+  A_less_B <= not(A) and B;
 
 end comparator_structural;
+
+-- Comparador de 8 bits
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity comparator_8bits is
+  port (
+        A,B: in std_logic_vector(7 downto 0); 
+        A_less_B: out std_logic 
+  );
+end comparator_8bits;
+
+architecture comparator_8bit_arch of comparator_8bits is
+
+  component comparator_VHDL is
+    port (
+          A,B: in std_logic; 
+          A_less_B: out std_logic 
+    );
+  end component;
+
+signal temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8: std_logic; 
+
+begin
+
+comp1: comparator_VHDL
+  port map(A(0),B(0),temp1);
+
+comp2: comparator_VHDL
+  port map(A(1),B(1),temp2);
+
+comp3: comparator_VHDL
+  port map(A(2),B(2),temp3);
+
+comp4: comparator_VHDL
+  port map(A(3),B(3),temp4);
+
+comp5: comparator_VHDL
+  port map(A(4),B(4),temp5);
+
+comp6: comparator_VHDL
+  port map(A(5),B(5),temp6);
+
+comp7: comparator_VHDL
+  port map(A(6),B(6),temp7);
+
+comp8: comparator_VHDL
+  port map(A(7),B(7),temp8);
+
+ A_less_B <= temp1 and temp2 and temp3 and temp4 and temp5 and temp6 and temp7 and temp8;
+
+end comparator_8bit_arch;
--- Comparador_TB.vhd
+++ Comparador_TB.vhd
@@ -1,48 +1,48 @@
--- fpga4student.com: FPGA projects, Verilog projects, VHDL projects 
--- VHDL project: VHDL code for a comparator 
--- Testbench VHDL code for comparator
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-use ieee.numeric_std.all; 
-ENTITY tb_comparator_VHDL IS
-END tb_comparator_VHDL;
- 
-ARCHITECTURE behavior OF tb_comparator_VHDL IS 
- 
-    -- Component Declaration for the comparator in VHDL
- 
-    COMPONENT comparator_VHDL
-    PORT(
-         A : IN  std_logic_vector(1 downto 0);
-         B : IN  std_logic_vector(1 downto 0);
-         A_less_B : OUT  std_logic
-        );
-    END COMPONENT;
-   --Inputs
-   signal A : std_logic_vector(1 downto 0) := (others => '0');
-   signal B : std_logic_vector(1 downto 0) := (others => '0');
-  --Outputs
-   signal A_less_B : std_logic;
+-- Testbench automatically generated online
+-- at https://vhdl.lapinoo.net
+-- Generation date : 22.3.2021 20:03:16 UTC
 
-BEGIN
- -- Instantiate the comparator in VHDL
-   uut: comparator_VHDL 
-   PORT MAP (
-          A => A,
-          B => B,
-          A_less_B => A_less_B
-        );
+library ieee;
+use ieee.std_logic_1164.all;
 
-   -- Stimulus process
-   stim_proc: process
-   begin 
-  -- create test cases for A_less_B
-  for i in 0 to 7 loop 
-           A <= std_logic_vector(to_unsigned(i,2));  
-           B <= std_logic_vector(to_unsigned(i+1,2));  
-           wait for 20 ns; 
-      end loop;
-      wait;
-   end process;
+entity tb_comparator_VHDL is
+end tb_comparator_VHDL;
 
-END;
+architecture tb of tb_comparator_VHDL is
+
+    component comparator_VHDL
+        port (A        : in std_logic;
+              B        : in std_logic;
+              A_less_B : out std_logic);
+    end component;
+
+    signal A        : std_logic;
+    signal B        : std_logic;
+    signal A_less_B : std_logic;
+
+begin
+
+    dut : comparator_VHDL
+    port map (A        => A,
+              B        => B,
+              A_less_B => A_less_B);
+
+    stimuli : process
+    begin
+        -- EDIT Adapt initialization as needed
+        A <= '0';
+        B <= '0';
+
+        -- EDIT Add stimuli here
+
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_comparator_VHDL of tb_comparator_VHDL is
+    for tb
+    end for;
+end cfg_tb_comparator_VHDL;
